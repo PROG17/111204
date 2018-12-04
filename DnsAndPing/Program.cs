5de478f699a015dab92cefa521460bcce7316902
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-12-04 10:45:43 +0100

    Add project files.

---
+++ DnsAndPing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnsAndPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
