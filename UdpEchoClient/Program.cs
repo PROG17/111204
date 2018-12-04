8a3861b738f71aa097e2e365ce72282229fb4b78
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-12-04 15:34:36 +0100

    Udp

---
+++ UdpEchoClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpEchoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UdpEchoClient");
+
+            Console.WriteLine("Ange host:");
+            var host = Console.ReadLine();
+            Console.WriteLine("Ange port:");
+            var port = int.Parse(Console.ReadLine());
+
+            var client = new UdpClient();
+
+            while (true)
+            {
+                Console.WriteLine("Ange text att skicka:");
+                var text = Console.ReadLine();
+
+                var data = Encoding.UTF8.GetBytes(text);
+
+                client.Send(data, data.Length, host, port);
+            }
+        }
+    }
+}
---
+++ UdpEchoServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpEchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UdpEchoServer");
+
+            Console.WriteLine("Ange port att lyssna på:");
+            var port = int.Parse(Console.ReadLine());
+
+            var remoteEndpoint = new IPEndPoint(IPAddress.Any, 0);
+
+            using (var client = new UdpClient(port))
+            {
+                while (true)
+                {
+                    var data = client.Receive(ref remoteEndpoint);
+                    var text = Encoding.UTF8.GetString(data);
+                    Console.WriteLine($"Endpoint: {remoteEndpoint} - {text}");
+                }
+            }
+        }
+    }
+}
