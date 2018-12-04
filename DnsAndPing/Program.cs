089b8cb267038146004e3f1b4270f80e0849a7be
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-12-04 11:12:24 +0100

    Ping and DNS

--- DnsAndPing/Program.cs
+++ DnsAndPing/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Net; // Innehåller det mesta av nätverksklasserna
+using System.Net.NetworkInformation;
 
 namespace DnsAndPing
 {
     class Program
     {
+        public static bool PingHost(IPAddress nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+
+            try
+            {
+                pinger = new Ping();
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+
+                Console.WriteLine($"Reply from {reply.Address} in {reply.RoundtripTime} ms.");
+
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            finally
+            {
+                if (pinger != null)
+                {
+                    pinger.Dispose();
+                }
+            }
+
+            return pingable;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS
+            var hostname = Dns.GetHostName();
+
+            Console.WriteLine($"GetHostName: {hostname}");
+
+            IPHostEntry entry = Dns.GetHostEntry("www.caribou.se");
+
+            Console.WriteLine($"GetHostEntry: {entry.HostName}");
+            foreach (IPAddress address in entry.AddressList)
+            {
+                Console.WriteLine($"  Address: {address}");
+
+                PingHost(address);
+            }
+
+
+
+
+
+
         }
     }
 }
