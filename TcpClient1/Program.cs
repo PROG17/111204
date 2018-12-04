7c01cb883045e8b0afa7b8d3f5e689db59e2222c
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-12-04 13:15:42 +0100

    TcpClient - Simple http client implementation

---
+++ TcpClient1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpClient1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TcpClient client = null;
+            NetworkStream networkStream = null;
+            StreamReader reader = null;
+            StreamWriter writer = null;
+
+            try
+            {
+                client = new TcpClient("www.caribou.se", 80); // Antingen try-finally eller using-statement.
+
+                networkStream = client.GetStream();
+
+                reader = new StreamReader(networkStream);
+                writer = new StreamWriter(networkStream);
+
+                writer.WriteLine("GET / HTTP/1.1");
+                writer.WriteLine("Host: www.caribou.se");
+                writer.WriteLine();
+                writer.Flush();
+
+                var result = reader.ReadToEnd();
+
+                Console.WriteLine(result);
+
+            }
+            finally
+            {
+                networkStream.Close();
+                client.Close();
+            }
+
+
+
+        }
+    }
+}
